6480a65aad10c622c33aba07ac030658595282e4
mik284 <mikeykamau222@gmail.com>
2023-07-06 13:08:17 +0300

    updated router

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
+import Staff from "../pages/staff/Staff";
 
 function Routers() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Staff />}/>
+        </Route>
       </Routes>
     </>
   );
