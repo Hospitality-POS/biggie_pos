0a3c212cba386f7276c2e8b0d1b39ab2668b323e
mik284 <mikeykamau222@gmail.com>
2023-07-07 15:48:11 +0300

    add staff and table routes

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Staff from "../pages/staff/Staff";
+import Table from "../pages/Tables/Table";
 
 function Routers() {
   return (
@@ -8,6 +9,8 @@ function Routers() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Staff />}/>
+          <Route path="/staff" element={<Staff />}/>
+          <Route path="/tables" element={<Table />}/>
         </Route>
       </Routes>
     </>
