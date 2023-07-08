733debdacb6498edbb6add76b6ea693395450ab1
mik284 <mikeykamau222@gmail.com>
2023-07-09 00:00:26 +0300

    modified routes

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Staff from "../pages/staff/Staff";
 import Table from "../pages/Tables/Table";
+import RestaurantPage from "../pages/Restaurant/Restuarant";
 
 function Routers() {
   return (
@@ -11,6 +12,7 @@ function Routers() {
           <Route index element={<Staff />}/>
           <Route path="/staff" element={<Staff />}/>
           <Route path="/tables" element={<Table />}/>
+          <Route path="/rest" element={<RestaurantPage />}/>
         </Route>
       </Routes>
     </>
