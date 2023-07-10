1f808a6656b636be8519e32253a110fc18114f67
mik284 <mikeykamau222@gmail.com>
2023-07-10 19:00:32 +0300

    add spinner lazy loading

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
-import Staff from "../pages/staff/Staff";
+// import Staff from "../pages/staff/Staff";
 import Table from "../pages/Tables/Table";
 import RestaurantPage from "../pages/Restaurant/Restuarant";
+import { Suspense, lazy } from "react";
+import Spinner from "../components/spinner/Spinner";
+
+const Staff = lazy(()=>import("../pages/staff/Staff"));
 
 function Routers() {
   return (
-    <>
+    <Suspense fallback={<Spinner />}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Staff />}/>
@@ -17,7 +21,7 @@ function Routers() {
           <Route path="/kitchen" element={""} />
         </Route>
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
