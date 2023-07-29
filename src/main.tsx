1b9e2893a7aba9b411a2c5cef8732a4931d38351
mik284 <mikeykamau222@gmail.com>
2023-07-29 17:16:54 +0300

    Add new font Inter

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,33 @@ import ReactDOM from "react-dom/client";
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
 import App from "./App.tsx";
+import "typeface-inter";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Inter, sans-serif",
+  },
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </BrowserRouter>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
