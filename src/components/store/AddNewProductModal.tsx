588b547065d29f3c7c9b0b1e1baddbfe664bf9f3
mik284 <mikeykamau222@gmail.com>
2023-08-21 07:50:42 +0300

    update modal to dialog

--- src/components/store/AddNewProductModal.tsx
+++ src/components/store/AddNewProductModal.tsx
@@ -1,23 +1,26 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import React, { useState } from "react";
 import {
+  Box,
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
   TextareaAutosize,
   Select,
   MenuItem,
   InputLabel,
   FormControl,
-  IconButton,
   Input,
   InputAdornment,
+  Typography,
+  Grid,
 } from "@mui/material";
+import { CloseRounded } from "@mui/icons-material";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { CloseRounded } from "@mui/icons-material";
 import { useDispatch } from "react-redux";
 import { createProduct } from "../../features/Product/ProductAction";
 
@@ -25,44 +28,23 @@ interface Category {
   _id: string;
   name: string;
 }
+
 interface AddNewProductModalProps {
   open: boolean;
   onClose: () => void;
-  onSave: (
-    name: string,
-    price: number,
-    description: string,
-    image: File | null,
-    quantity: number,
-    minViableQuantity: number,
-    category: string
-  ) => void;
 }
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 900,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  borderRadius: 2,
-  p: 4,
-};
 
 const AddNewProductModal: React.FC<AddNewProductModalProps> = ({
   open,
   onClose,
-  onSave,
 }) => {
-  const [name, setName] = React.useState("");
-  const [price, setPrice] = React.useState(0);
-  const [desc, setDescription] = React.useState("");
-  const [image, setImage] = React.useState<File | null>(null);
-  const [quantity, setQuantity] = React.useState(0);
-  const [min_viable_quantity, setMinViableQuantity] = React.useState(0);
-  const [category, setCategory] = React.useState<string>("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [desc, setDescription] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+  const [quantity, setQuantity] = useState(0);
+  const [min_viable_quantity, setMinViableQuantity] = useState(0);
+  const [category, setCategory] = useState<string>("");
 
   const fetchCategories = async () => {
     const response = await axios.get<Category[]>(
@@ -74,14 +56,16 @@ const AddNewProductModal: React.FC<AddNewProductModalProps> = ({
   const { data: categories } = useQuery<Category[]>(["categories"], () =>
     fetchCategories()
   );
+
   const dispatch = useDispatch();
+
   const handleSave = async () => {
     const productData = {
       name,
       price,
       description: desc,
       quantity,
-      min_viable_quantity: min_viable_quantity,
+      min_viable_quantity,
       category,
       image: image || null,
     };
@@ -110,85 +94,72 @@ const AddNewProductModal: React.FC<AddNewProductModalProps> = ({
   };
 
   return (
-    <div>
-      <Modal
-        keepMounted
-        open={open}
-        onClose={onClose}
-        aria-labelledby="add new product"
-        aria-describedby="New product add to store"
+    <Dialog open={open} maxWidth="md" onClose={onClose}>
+      <DialogTitle
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignContent: "center",
+          backgroundColor: "#6c1c2c",
+          color: "white",
+        }}
       >
-        <Box sx={style}>
-          <div
-            style={{
-              backgroundColor: "#6c1c2c",
-              padding: "8px",
-              marginBottom: "16px",
-              borderRadius: "4px",
-              textAlign: "center",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-            // encType="multipart/form-data"
-          >
-            <Typography variant="h6" component="h2" color="white">
-              Add a new Dish
-            </Typography>
-            <IconButton onClick={() => onClose()}>
-              <CloseRounded fontSize="large" color="inherit" />
-            </IconButton>
-          </div>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                fullWidth
-                margin="normal"
-              />
-              <FormControl fullWidth margin="normal">
-                <InputLabel id="category">Category</InputLabel>
-                <Select
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value as string)}
-                  label="Category"
-                >
-                  <MenuItem value="">Select category</MenuItem>
-                  {categories &&
-                    categories.map(
-                      (category: {
-                        _id: React.Key | readonly string[] | null | undefined;
-                        name:
-                          | string
-                          | number
-                          | boolean
-                          | React.ReactElement<
-                              any,
-                              string | React.JSXElementConstructor<any>
-                            >
-                          | Iterable<React.ReactNode>
-                          | React.ReactPortal
-                          | null
-                          | undefined;
-                      }) => (
-                        <MenuItem key={category._id} value={category._id}>
-                          {category.name}
-                        </MenuItem>
-                      )
-                    )}
-                </Select>
-              </FormControl>
-              <TextField
-                label="Price"
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(parseFloat(e.target.value))}
-                fullWidth
-                margin="normal"
-              />
-              {/* <div
+        Add a New Dish{" "}
+        <IconButton onClick={onClose}>
+          <CloseRounded fontSize="inherit" color="inherit" />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="category">Category</InputLabel>
+              <Select
+                value={category}
+                onChange={(e) => setCategory(e.target.value as string)}
+                label="Category"
+              >
+                <MenuItem value="">Select category</MenuItem>
+                {categories &&
+                  categories.map(
+                    (category: {
+                      _id: React.Key | readonly string[] | null | undefined;
+                      name:
+                        | string
+                        | number
+                        | boolean
+                        | React.ReactElement<
+                            any,
+                            string | React.JSXElementConstructor<any>
+                          >
+                        | Iterable<React.ReactNode>
+                        | React.ReactPortal
+                        | null
+                        | undefined;
+                    }) => (
+                      <MenuItem key={category._id} value={category._id}>
+                        {category.name}
+                      </MenuItem>
+                    )
+                  )}
+              </Select>
+            </FormControl>
+            <TextField
+              label="Price"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(parseFloat(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            {/* <div
                 style={{
                   display: "flex",
                   flexDirection: "column",
@@ -202,72 +173,76 @@ const AddNewProductModal: React.FC<AddNewProductModalProps> = ({
                   onChange={handleImageChange}
                 />
               </div> */}
-              <FormControl fullWidth margin="normal">
-                {/* <InputLabel htmlFor="image">Image</InputLabel> */}
-                <Input
-                  type="file"
-                  id="image"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <label htmlFor="image">
-                        <IconButton component="span">
-                          <CloseRounded fontSize="small" />
-                        </IconButton>
-                      </label>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Quantity"
-                type="number"
-                value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value))}
-                fullWidth
-                margin="normal"
+            <FormControl fullWidth margin="normal">
+              {/* <InputLabel htmlFor="image">Image</InputLabel> */}
+              <Input
+                type="file"
+                id="image"
+                accept="image/*"
+                onChange={handleImageChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <label htmlFor="image">
+                      <IconButton component="span">
+                        <CloseRounded fontSize="small" />
+                      </IconButton>
+                    </label>
+                  </InputAdornment>
+                }
               />
-              <TextField
-                label="Min Viable Quantity"
-                type="number"
-                value={min_viable_quantity}
-                onChange={(e) => setMinViableQuantity(parseInt(e.target.value))}
-                fullWidth
-                margin="normal"
-              />
-              <TextareaAutosize
-                placeholder="Description"
-                value={desc}
-                onChange={(e) => setDescription(e.target.value)}
-                minRows={3}
-                maxRows={3}
-                style={{ width: "100%", marginTop: 10 }}
-              />
-            </Grid>
+            </FormControl>
           </Grid>
-          <Button
-            onClick={handleSave}
-            variant="outlined"
-            sx={{
-              pl: 2,
-              color: "#6c1c2c",
-              borderColor: "#6c1c2c",
+          <Grid item xs={6}>
+            <TextField
+              label="Quantity"
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Min Viable Quantity"
+              type="number"
+              value={min_viable_quantity}
+              onChange={(e) => setMinViableQuantity(parseInt(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            <TextareaAutosize
+              placeholder="Description"
+              value={desc}
+              onChange={(e) => setDescription(e.target.value)}
+              minRows={3.5}
+              maxRows={3.5}
+              style={{ width: "100%", marginTop: 15 }}
+            />
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        {/* <Button onClick={onClose} color="primary">
+          Cancel
+        </Button> */}
+        <Button
+          variant="outlined"
+          onClick={handleSave}
+          sx={{
+            pl: 2,
+            color: "#6c1c2c",
+            borderColor: "#6c1c2c",
 
-              "&:hover": {
-                borderColor: "#bc8c7c",
-                color: "#bc8c7c",
-              },
-            }}
-            fullWidth
-          >
-            Save
-          </Button>
-        </Box>
-      </Modal>
-    </div>
+            "&:hover": {
+              borderColor: "#bc8c7c",
+              color: "#bc8c7c",
+            },
+          }}
+          fullWidth
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
--- src/components/store/EditProductModal.tsx
+++ src/components/store/EditProductModal.tsx
@@ -1,76 +1,57 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import React, { useState } from "react";
 import {
+  Box,
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
   TextareaAutosize,
   Select,
   MenuItem,
   InputLabel,
   FormControl,
-  IconButton,
+  Grid,
 } from "@mui/material";
+import { CloseRounded } from "@mui/icons-material";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { CloseRounded } from "@mui/icons-material";
 import { useDispatch } from "react-redux";
 import { updateProduct } from "../../features/Product/ProductAction";
+
+interface Category {
+  _id: string;
+  name: string;
+}
 
 interface EditProductModalProps {
   productData: any;
   open: boolean;
   onClose: () => void;
-  onSave: (
-    name: string,
-    price: number,
-    description: string,
-    image: File | null,
-    quantity: number,
-    minViableQuantity: number,
-    category: string
-  ) => void;
 }
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 900,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  borderRadius: 2,
-  p: 4,
-};
 
 const EditProductModal: React.FC<EditProductModalProps> = ({
   open,
   productData,
   onClose,
 }) => {
-  const [name, setName] = React.useState(productData.name || "");
-  const [price, setPrice] = React.useState(productData.price || 0);
-  const [desc, setDescription] = React.useState(productData.desc || "");
-  const [image, setImage] = React.useState<File | null>(null);
-  const [quantity, setQuantity] = React.useState(productData.quantity || 0);
-  const [min_viable_quantity, setMinViableQuantity] = React.useState(
+  const [name, setName] = useState(productData.name || "");
+  const [price, setPrice] = useState(productData.price || 0);
+  const [desc, setDescription] = useState(productData.desc || "");
+  const [image, setImage] = useState<File | null>(null);
+  const [quantity, setQuantity] = useState(productData.quantity || 0);
+  const [min_viable_quantity, setMinViableQuantity] = useState(
     productData.min_viable_quantity || 0
   );
-  const [category, setCategory] = React.useState(
-    productData?.category?._id || ""
-  );
-
+  const [category, setCategory] = useState(productData?.category?._id || "");
 
   const dispatch = useDispatch();
   const fetchCategories = async () => {
     const response = await axios.get("http://localhost:3000/categories");
     return response.data;
   };
-
-  //   console.log(productData);
 
   const { data: categories } = useQuery(
     ["categories"],
@@ -127,146 +108,136 @@ const EditProductModal: React.FC<EditProductModalProps> = ({
   };
 
   return (
-    <div>
-      <Modal
-        keepMounted
-        open={open}
-        onClose={onClose}
-        aria-labelledby="add new product"
-        aria-describedby="New product add to store"
+    <Dialog open={open} maxWidth="md" onClose={onClose}>
+      <DialogTitle
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignContent: "center",
+          backgroundColor: "#6c1c2c",
+          color: "white",
+        }}
       >
-        <Box sx={style}>
-          <div
-            style={{
-              backgroundColor: "#6c1c2c",
-              padding: "8px",
-              marginBottom: "16px",
-              borderRadius: "4px",
-              textAlign: "center",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6" component="h2" color="white">
-              Update Dish
-            </Typography>
-            <IconButton onClick={() => onClose()}>
-              <CloseRounded fontSize="large" color="inherit" />
-            </IconButton>
-          </div>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                fullWidth
-                margin="normal"
-              />
-              <FormControl fullWidth margin="normal">
-                <InputLabel id="category">Category</InputLabel>
-                <Select
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value as string)}
-                  label="Category"
-                >
-                  <MenuItem value="">Select category</MenuItem>
-                  {categories &&
-                    categories.map(
-                      (category: {
-                        _id: React.Key | readonly string[] | null | undefined;
-                        name:
-                          | string
-                          | number
-                          | boolean
-                          | React.ReactElement<
-                              any,
-                              string | React.JSXElementConstructor<any>
-                            >
-                          | Iterable<React.ReactNode>
-                          | React.ReactPortal
-                          | null
-                          | undefined;
-                      }) => (
-                        <MenuItem key={category._id} value={category._id}>
-                          {category.name}
-                        </MenuItem>
-                      )
-                    )}
-                </Select>
-              </FormControl>
-              <TextField
-                label="Price"
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(parseFloat(e.target.value))}
-                fullWidth
-                margin="normal"
-              />
-
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marginBottom: 20,
-                }}
+        Update Dish{" "}
+        <IconButton onClick={onClose}>
+          <CloseRounded fontSize="inherit" color="inherit" />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="category">Category</InputLabel>
+              <Select
+                value={category}
+                onChange={(e) => setCategory(e.target.value as string)}
+                label="Category"
               >
-                <label htmlFor="image">Image</label>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                />
-              </div>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Quantity"
-                type="number"
-                value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value))}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="Min Viable Quantity"
-                type="number"
-                value={min_viable_quantity}
-                onChange={(e) => setMinViableQuantity(parseInt(e.target.value))}
-                fullWidth
-                margin="normal"
-              />
-              <TextareaAutosize
-                placeholder="Description"
-                value={desc}
-                onChange={(e) => setDescription(e.target.value)}
-                minRows={3}
-                maxRows={3}
-                style={{ width: "100%", marginTop: 10 }}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            onClick={handleUpdate}
-            variant="outlined"
-            sx={{
-              pl: 2,
-              color: "#6c1c2c",
-              borderColor: "#6c1c2c",
+                <MenuItem value="">Select category</MenuItem>
+                {categories &&
+                  categories.map(
+                    (category: {
+                      _id: React.Key | readonly string[] | null | undefined;
+                      name:
+                        | string
+                        | number
+                        | boolean
+                        | React.ReactElement<
+                            any,
+                            string | React.JSXElementConstructor<any>
+                          >
+                        | Iterable<React.ReactNode>
+                        | React.ReactPortal
+                        | null
+                        | undefined;
+                    }) => (
+                      <MenuItem key={category._id} value={category._id}>
+                        {category.name}
+                      </MenuItem>
+                    )
+                  )}
+              </Select>
+            </FormControl>
+            <TextField
+              label="Price"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(parseFloat(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
 
-              "&:hover": {
-                borderColor: "#bc8c7c",
-                color: "#bc8c7c",
-              },
-            }}
-            fullWidth
-          >
-            update
-          </Button>
-        </Box>
-      </Modal>
-    </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                marginBottom: 20,
+              }}
+            >
+              <label htmlFor="image">Image</label>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Quantity"
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Min Viable Quantity"
+              type="number"
+              value={min_viable_quantity}
+              onChange={(e) => setMinViableQuantity(parseInt(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            <TextareaAutosize
+              placeholder="Description"
+              value={desc}
+              onChange={(e) => setDescription(e.target.value)}
+              minRows={3.5}
+              maxRows={3.5}
+              style={{ width: "100%", marginTop: 15 }}
+            />
+          </Grid>
+        </Grid>
+        
+      </DialogContent>
+      <DialogActions>
+        {/* <Button onClick={onClose} color="primary">
+          Cancel
+        </Button> */}
+        <Button onClick={handleUpdate}  variant="outlined"
+          sx={{
+            pl: 2,
+            color: "#6c1c2c",
+            borderColor: "#6c1c2c",
+
+            "&:hover": {
+              borderColor: "#bc8c7c",
+              color: "#bc8c7c",
+            },
+          }}
+          fullWidth>
+          Update
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
