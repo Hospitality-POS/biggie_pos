23a2329aa396d0b1bbd74cc265fe11675ef43212
mik284 <mikeykamau222@gmail.com>
2023-09-05 18:57:57 +0300

    add reports page and route

---
+++ src/pages/Settings/Reports.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Tab,
+  Tabs,
+  Typography,
+  Paper,
+  Grid,
+  Button,
+  TextField,
+} from "@mui/material";
+import FilterListIcon from '@mui/icons-material/FilterList';
+
+// Dummy data for reports
+const generateReport = (type: string, startDate: string, endDate: string) => {
+  // You can replace this with actual report generation logic
+  return `Generated ${type} report from ${startDate} to ${endDate}.`;
+};
+
+const Reports: React.FC = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [report, setReport] = useState<string | null>(null);
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+  };
+
+  const handleStartDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDate(event.target.value);
+  };
+
+  const generateReportHandler = () => {
+     if (!startDate || !endDate) {
+      return;
+    }
+    if (activeTab === 0) {
+      setReport(generateReport("Sale", startDate, endDate));
+    } else if (activeTab === 1) {
+      setReport(generateReport("Purchase", startDate, endDate));
+    } else if (activeTab === 2) {
+      setReport(generateReport("Suppliers", startDate, endDate));
+    }
+    // Add more conditions for other report types as needed
+  };
+
+
+  const isGenerateButtonDisabled = !startDate || !endDate;
+  return (
+    <Grid container spacing={2} padding={2}>
+      <Grid item xs={8}>
+        <Typography variant="h5" gutterBottom>Generate Reports</Typography>
+        <AppBar position="static" color="default">
+          <Tabs value={activeTab} onChange={handleTabChange}>
+            <Tab label="Sale" />
+            <Tab label="Purchase" />
+            <Tab label="Suppliers" />
+          </Tabs>
+        </AppBar>
+       
+        <Box mt={2}>
+          <Button variant="outlined" onClick={generateReportHandler} disabled={isGenerateButtonDisabled}>
+            Generate Report
+          </Button>
+        </Box>
+        <Box mt={2}>
+          {report && (
+            <Paper>
+              {/* Display the report here */}
+              <Typography variant="h6">Report Preview</Typography>
+              <Typography variant="body1">{report}</Typography>
+            </Paper>
+          )}
+        </Box>
+      </Grid>
+      <Grid item xs={4} marginTop={5}>
+        {/* Filters */}
+        <Paper>
+          {/* Add your filter components here */}
+          <Typography variant="h6" paddingLeft={2} gutterBottom sx={{display: "flex", alignItems: "center", gap:1}}>
+            <FilterListIcon/>
+            Filters
+          </Typography>
+          {/* Add filter components */}
+          <Box mt={2} sx={{ display: "flex", gap: 2, paddingLeft: 2 , paddingBottom: 2}}>
+            <TextField
+              label="Start Date"
+              type="date"
+              value={startDate}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={handleStartDateChange}
+              variant="outlined"
+            />
+            <TextField
+              label="End Date"
+              type="date"
+              value={endDate}
+              onChange={handleEndDateChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+          </Box>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Reports;
--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -6,6 +6,7 @@ import UsersList from "../pages/Settings/UsersList";
 import Staff from "../pages/staff/Staff";
 import Table from "../pages/Tables/Table";
 import Inventory from "../pages/Settings/Inventory";
+import Reports from "../pages/Settings/Reports";
 
 const Layout = lazy(() => import("../components/layout/Layout"));
 // const Staff = lazy(() => import("../pages/staff/Staff"));
@@ -25,6 +26,7 @@ function Routers() {
           <Route path="/restaurant/:id" element={<Private><RestaurantPage /></Private>} />
           <Route path="/store" element={<Private><Store /></Private>} />
           <Route path="/users" element={<Private><UsersList /></Private>} />
+          <Route path="/reports" element={<Private><Reports /></Private>} />
           <Route path="/Inventory" element={<Private><Inventory /></Private>} />
           <Route path="/Orders" element={<Private><Orders /></Private>} />
           <Route path="*" element={<Spinner />} />
