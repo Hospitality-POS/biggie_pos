2bac98acab560ede2207b8a1f516218c99c096bd
mik284 <mikeykamau222@gmail.com>
2023-12-23 23:30:23 +0300

    updated categories url

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,11 +1,33 @@
+import { ParamsType } from "@ant-design/pro-components";
 import axios from "axios";
 // get all categories
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const fetchAllCategories = async () => {
-  const response = await axios.get(`${BASE_URL}/categories`);
+export const fetchAllCategories = async (
+  data: ParamsType & {
+    pageSize?: number | undefined;
+    current?: number | undefined;
+    keyword?: string | undefined;
+  }
+) => {
+  let url = `${BASE_URL}/categories`;
+
+  // Append query parameters if provided
+  if (data.name) {
+    url += `?name=${encodeURIComponent(data.name)}`;
+  }
+
+  if (data.sub_category) {
+    url += `${data.name ? "&" : "?"}sub_category=${encodeURIComponent(
+      data.sub_category?.name
+    )}`;
+  }
+
+  const response = await axios.get(url);
   return response.data;
+
+  // const response = await axios.get(`${BASE_URL}/categories`);
+  // return response.data;
 };
 
-
-// FILTER ON BACKEND TO PREVENT 
+// FILTER ON BACKEND TO PREVENT
