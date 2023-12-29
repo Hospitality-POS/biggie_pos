0152bd3b520c72b3d07bceebc652da9d7927143f
mik284 <mikeykamau222@gmail.com>
2023-12-29 03:08:37 +0300

    updated userlist and routes for main sup settings

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -7,7 +7,7 @@ import Staff from "../pages/staff/Staff";
 import Table from "../pages/Tables/Table";
 import Inventory from "../pages/Settings/Inventory";
 import Reports from "../pages/Settings/Reports";
-import SupplierTable from "../pages/Settings/Supplier";
+import SupplierTable from "../pages/Settings/supplierLevel/Supplier";
 import MainCategory from "../pages/main_category/Main_category";
 import TableSettings from "../pages/Settings/TableLevel/TableSettings";
 import CategorySettings from "../pages/Settings/categoryLevel/CategorySettings";
@@ -15,6 +15,7 @@ import PaymentSettings from "../pages/Settings/PaymentSettings";
 import CategoryMainSettings from "../pages/Settings/categoryLevel/Category_main_settings";
 import EmptyPage from "./EmptyPage";
 import TableMainSettings from "../pages/Settings/TableLevel/Table_main_settings";
+import SupplierMainSettings from "../pages/Settings/supplierLevel/supplier_main_settings";
 
 
 const Layout = lazy(() => import("../components/layout/Layout"));
@@ -36,7 +37,7 @@ function Routers() {
           <Route path="/store" element={<Private><Store/></Private>} />
           <Route path="/users" element={<Private><UsersList /></Private>} />
           <Route path="/reports" element={<Private><Reports /></Private>} />
-          <Route path="/supplier" element={<Private><SupplierTable /></Private>} />
+          <Route path="/supplier" element={<Private><SupplierMainSettings /></Private>} />
           <Route path="/table-settings" element={<Private><TableMainSettings /></Private>} />
           <Route path="/category-settings" element={<Private><CategoryMainSettings /></Private>} />
           <Route path="/payment-settings" element={<Private><PaymentSettings /></Private>} />
