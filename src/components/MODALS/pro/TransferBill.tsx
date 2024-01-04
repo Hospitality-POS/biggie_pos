24412b991e1ff9ae39920054965ef3ae9e6c4c5a
mik284 <mikeykamau222@gmail.com>
2024-01-04 13:49:15 +0300

    create transfer bill

---
+++ src/components/MODALS/pro/TransferBill.tsx
@@ -0,0 +1,134 @@
+import React, { useRef } from "react";
+import { Button, Form, Space } from "antd";
+import { ModalForm, ProForm, ProFormDigit } from "@ant-design/pro-form";
+import { DollarCircleOutlined, SwapOutlined, TableOutlined } from "@ant-design/icons";
+import { transferBill } from "@services/bills";
+import ShowConfirm from "@utils/ConfirmUtil";
+import { ProFormSelect, ProFormTreeSelect } from "@ant-design/pro-components";
+import { fetchTableUsequery } from "@services/tables";
+import { useQuery } from "@tanstack/react-query";
+import { useAppSelector } from "src/store";
+
+interface TransferBillModalProps {
+  data?: any;
+}
+
+const TransferBillModal: React.FC<TransferBillModalProps> = ({ data : cartItem }) => {
+  const { data } = useQuery({
+    queryKey: ["tables"],
+    queryFn: fetchTableUsequery,
+    retry: 3,
+    refetchInterval: 3000,
+    networkMode: "always",
+  });
+  const [form] = Form.useForm();
+  const formRef = useRef();
+
+  const locations = data.map((table: { name: string; _id: string; tables: any[]; }) => ({
+    title: table.name,
+    value: table._id,
+    children: table.tables
+      .filter((childTable: { isOccupied: boolean; }) => !childTable.isOccupied) 
+      .map((childTable: { name: string; _id: string; }) => ({
+        title: childTable.name,
+        value: childTable._id,
+      })),
+  }));
+
+  const productOptions = cartItem?.map((product) => ({
+    label: product.product_id?.name,
+    value: product._id,
+  }));
+  
+  return (
+    <ModalForm
+      form={form}
+      formRef={formRef}
+      title={
+        <Space>
+          <SwapOutlined />
+          {"Transfer Bill"}
+        </Space>
+      }
+      trigger={
+        <Button
+          style={{
+            pl: 2,
+            color: "#6c1c2c",
+            borderColor: "#6c1c2c",
+            "&:hover": {
+              borderColor: "#bc8c7c",
+              color: "#bc8c7c",
+            },
+          }}
+          icon={<SwapOutlined />}
+        >
+          Transfer Bill
+        </Button>
+      }
+      autoFocusFirstInput
+      modalProps={{
+        destroyOnClose: true,
+        style: { display: "grid", placeContent: "center" },
+      }}
+      onFinish={async (values) => {
+        const confirmed = await ShowConfirm({
+          title: "Are you sure you want to transfer this bill?",
+        });
+        if (confirmed) {
+          //   await transferBill(values);
+          //   actionRef.current.reset();
+          return true;
+        }
+      }}
+      submitter={{
+        searchConfig: {
+          resetText: "Cancel",
+          submitText: "Transfer Bill",
+        },
+      }}
+    >
+      <ProForm.Group>
+        <ProFormSelect
+          width={"md"}
+          label="Products"
+          name="products"
+          mode="multiple"
+          options={productOptions}
+          rules={[
+            { required: true, message: "Please select one or more products" },
+          ]}
+        />
+
+        <ProFormTreeSelect
+          width={"md"}
+          name="table"
+          label="Tables"
+          rules={[
+            { required: true, message: "Please select one or more tables" },
+          ]}
+          request={() => locations}
+          allowClear
+          fieldProps={{
+            treeLine: true,
+            suffixIcon: <TableOutlined />,
+            filterTreeNode: true,
+            showSearch: true,
+            popupMatchSelectWidth: false,
+            labelInValue: true,
+            autoClearSearchValue: true,
+            multiple: true,
+            treeNodeFilterProp: "title",
+            fieldNames: {
+              label: "title",
+            },
+            getPopupContainer: () => document.body,
+          }}
+          style={{ width: "100%" }}
+        />
+      </ProForm.Group>
+    </ModalForm>
+  );
+};
+
+export default TransferBillModal;
