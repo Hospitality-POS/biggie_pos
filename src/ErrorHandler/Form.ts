ef46dd591cf69db08c134993e6dc76581154250d
mik284 <mikeykamau222@gmail.com>
2024-01-04 22:06:37 +0300

    removed unused form error handler

--- src/ErrorHandler/Form.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-const validationSchema: Yup.Schema<{ pin: string }> = Yup.object({
-  pin: Yup.string()
-    .required('PIN is required')
-    .matches(/^\d{4}$/, 'PIN must be exactly 4 digits'),
-});
-
-export default validationSchema;
