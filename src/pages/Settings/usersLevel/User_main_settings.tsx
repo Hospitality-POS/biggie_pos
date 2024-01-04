09b277eebc8f39321a5b8c9a42d73266d57d4367
mik284 <mikeykamau222@gmail.com>
2024-01-04 15:09:09 +0300

    add procard

---
+++ src/pages/Settings/usersLevel/User_main_settings.tsx
@@ -0,0 +1,29 @@
+import { ProCard } from "@ant-design/pro-components";
+import { UsergroupAddOutlined } from "@ant-design/icons";
+import { Space } from "antd/lib";
+import UsersList from "../UsersList";
+
+function UsersMainSettings() {
+  const tabsItems = [
+    {
+      key: "table1",
+      tab: "Table",
+      label: "All Users",
+      children: <UsersList />,
+    },
+  ];
+  return (
+    <>
+      <ProCard
+        style={{ height: "90vh" }}
+        title={<Space><UsergroupAddOutlined/>Users Main Settings</Space>}
+        tabs={{
+          type: "card",
+          items: tabsItems,
+        }}
+      />
+    </>
+  );
+}
+
+export default UsersMainSettings;
