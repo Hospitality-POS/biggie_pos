1bafb632bc5c8d8e36a00c7d54c6ea69b04dc078
mik284 <mikeykamau222@gmail.com>
2024-01-05 14:31:01 +0300

    create update business profile

--- src/pages/Settings/systemSetup/Profile.tsx
+++ src/pages/Settings/systemSetup/Profile.tsx
@@ -4,15 +4,32 @@ import {
   ProFormInstance,
   ProFormSelect,
   ProFormText,
+  ProSkeleton,
 } from "@ant-design/pro-components";
 import { getPhoneNumber } from "@components/PhoneNumber/utils/formatPhoneNumberUtil";
 import { PhoneInput } from "@components/PhoneNumber/PhoneNumber";
 import { updateUsers } from "@services/users";
 import ShowConfirm from "@utils/ConfirmUtil";
-import { Space, Typography } from "antd";
-import { useRef, useState } from "react";
+import { Form, Skeleton, Space, Spin, Typography } from "antd";
+import { useEffect, useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import {
+  createSystemSetup,
+  fetchSystemSetupDetails,
+  fetchSystemSetupDetailsById,
+  updateSystemSetup,
+} from "@services/systemsetup";
+import { reversePhoneNumber } from "@components/PhoneNumber/utils/reversePhoneNumberFormat";
 
 function SystemSetup() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["systemsettings"],
+    queryFn: fetchSystemSetupDetailsById,
+    // retry: 3,
+    // refetchInterval: 3000,
+    networkMode: "always",
+  });
+
   const [ShowPaybilldetails, setShowPaybilldetails] = useState(false);
   const formRef = useRef<ProFormInstance>();
   const formStyle = {
@@ -21,7 +38,7 @@ function SystemSetup() {
     flexWrap: "wrap",
     justifyContent: "space-evenly",
     width: "100%",
-    height: "calc(100vh - 320px)",
+    height: "calc(100vh - 300px)",
     overflowY: "auto",
   };
 
@@ -31,101 +48,124 @@ function SystemSetup() {
   };
 
   const onPaymentDetailsChange = (value: string) => {
-    console.log(value);
-
     value === "Paybill"
       ? setShowPaybilldetails(true)
       : setShowPaybilldetails(false);
   };
+  const [form] = Form.useForm();
+
 
   return (
-    <ProForm
-      layout="vertical"
-      grid
-      onFinish={async (values) => {
-        const phoneNumber = getPhoneNumber(values?.phoneNumber);
-        const data2 = { ...values, phone: phoneNumber };
-        formRef?.current?.resetFields();
-        return true;
-      }}
-      submitter={{
-        searchConfig: {
-          resetText: "Reset",
-          submitText: "Submit",
-        },
-        render: (_, dom) => (
-          <Space style={{ justifyContent: "flex-end", width: "100%" }}>
-            {dom}
-          </Space>
-        ),
-      }}
-    >
-      <div style={formStyle}>
-        <Typography.Text>Business Profile</Typography.Text>
-        <div style={fieldStyle}>
-          <ProFormText name="name" label="Business Name" />
-          <ProFormText
-            name="email"
-            label="Email"
-            rules={[{ required: true, message: "Please provide the email" }]}
-          />
-          <PhoneInput label="Phone" owner="phoneNumber" />
-          <ProFormText name="location" label="Location" />
-          <ProFormText name="social_link" label="Social Link" />
-        </div>
-        <div style={fieldStyle}>
-          <ProFormText name="kra_pin" label="KRA Pin" />
-          <ProFormSelect
-            name="paymentDetails"
-            label="Mpesa Payment Details"
-            options={[
-              { value: "Till", label: "Till" },
-              { value: "Paybill", label: "Paybill" },
-            ]}
-            initialValue={"Till"}
-            getValueFromEvent={(_, tr) => {
-              onPaymentDetailsChange(tr.value);
-              return onPaymentDetailsChange(tr.value);
-            }}
-          />
-          {ShowPaybilldetails ? (
-            <>
-              <ProFormDigit
-                name="account_no"
-                label="Account No."
+    <>
+      {isLoading ? (
+        <ProSkeleton type="descriptions" />
+      ) : (
+        <ProForm
+          form={form}
+          layout="vertical"
+          grid
+          loading={isLoading}
+          formRef={formRef}
+          initialValues={
+            data ? { ...data, phoneNumber: reversePhoneNumber(data.phone) } : {}
+          }
+          onFinish={async (values) => {
+            const phoneNumber = getPhoneNumber(values?.phoneNumber);
+            const data2 = { ...values, phone: phoneNumber };
+            // console.log("mmmmm", values);
+            
+            const confirmed = await ShowConfirm({
+              title: `Are you sure you want to ${
+                data ? "Update" : "Add new"
+              } system setup details?`,
+            });
+            if (confirmed) {
+              data
+                ? await updateSystemSetup({ data2, _id: data?._id })
+                : await createSystemSetup(data2);
+              return true;
+            }
+
+            return true;
+          }}
+          submitter={{
+            searchConfig: {
+              resetText: "Reload",
+              submitText: data ? "Update" : "Submit",
+            },
+            render: (_, dom) => (
+              <Space style={{ justifyContent: "flex-end", width: "100%" }}>
+                {dom}
+              </Space>
+            ),
+          }}
+        >
+          <div style={formStyle}>
+            <Typography.Text>Business Profile</Typography.Text>
+            <div style={fieldStyle}>
+              <ProFormText name="name" label="Business Name" />
+              <ProFormText
+                name="email"
+                label="Email"
                 rules={[
-                  {
-                    required: true,
-                    message: "Please provide the account number.",
-                  },
+                  { required: true, message: "Please provide the email" },
                 ]}
               />
-              <ProFormDigit
-                name="business_no"
-                label="Business No."
-                rules={[
-                  {
-                    required: true,
-                    message: "Please provide the business number.",
-                  },
+              <PhoneInput label="Phone" owner="phoneNumber" />
+              <ProFormText name="location" label="Location" />
+            </div>
+            <div style={fieldStyle}>
+              <ProFormText name="social_link" label="Social Link" />
+              <ProFormText name="kra_pin" label="KRA Pin" />
+              <ProFormSelect
+                name="paymentDetails"
+                label="Mpesa Payment Details"
+                options={[
+                  { value: "Till", label: "Till" },
+                  { value: "Paybill", label: "Paybill" },
                 ]}
+                getValueFromEvent={(_, tr) => onPaymentDetailsChange(tr.value)}
               />
-            </>
-          ) : (
-            <ProFormDigit
-              name="till_no"
-              label="Till No."
-              rules={[
-                {
-                  required: true,
-                  message: "Please provide the till number.",
-                },
-              ]}
-            />
-          )}
-        </div>
-      </div>
-    </ProForm>
+              {ShowPaybilldetails ? (
+                <>
+                  <ProFormDigit
+                    name="account_no"
+                    label="Account No."
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please provide the account number.",
+                      },
+                    ]}
+                  />
+                  <ProFormDigit
+                    name="business_no"
+                    label="Business No."
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please provide the business number.",
+                      },
+                    ]}
+                  />
+                </>
+              ) : (
+                <ProFormDigit
+                  name="till_no"
+                  label="Till No."
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please provide the till number.",
+                    },
+                  ]}
+                />
+              )}
+            </div>
+          </div>
+        </ProForm>
+      )}
+    </>
   );
 }
 
