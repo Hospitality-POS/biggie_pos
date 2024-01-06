6ac1783db3100f711bc559fc3008aab89da504cf
mik284 <mikeykamau222@gmail.com>
2024-01-06 21:05:38 +0300

    updated error dialog for store

--- src/components/MODALS/Dialogs/ErrorDialog.tsx
+++ src/components/MODALS/Dialogs/ErrorDialog.tsx
@@ -1,48 +1,21 @@
-import React from "react";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import ErrorIcon from "@mui/icons-material/Error";
-
-const dialogTitleStyle = {
-  position: "relative", // To make the CloseIcon position relative to the title
-};
-
-const errorIconStyle = {
-  margin: "0 auto",
-  fontSize: "60px", 
-  color: "red",
-  display: "block",
-  textAlign: "center", // To center the icon
-};
-
-const closeIconStyle = {
-  position: "absolute",
-  right: "8px",
-  top: "8px",
-  color: "grey",
-};
+import { Button, Result } from "antd/lib";
 
 const ErrorDialog = ({ error, onClose }) => {
   return (
-    <Dialog open={error !== null} onClose={onClose}>
-      <DialogTitle style={dialogTitleStyle}>
-      </DialogTitle>
-      <DialogContent>
-        <ErrorIcon style={errorIconStyle} />
-        <DialogContentText>{error}</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Close
+    <Result
+      status="500"
+      subTitle={
+        <>
+          <p>Check your internet connection!</p> <i>{error}</i>
+        </>
+      }
+      extra={
+        <Button type="primary" onClick={onClose}>
+          Reload page
         </Button>
-      </DialogActions>
-    </Dialog>
+      }
+      style={{ margin: "0 auto" }}
+    />
   );
 };
 
