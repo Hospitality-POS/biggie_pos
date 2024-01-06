371049ab695f56fa578f29fff8ff5ac13bf0ac06
mik284 <mikeykamau222@gmail.com>
2024-01-06 21:19:51 +0300

    update error :store

--- src/components/MODALS/Dialogs/ErrorDialog.tsx
+++ src/components/MODALS/Dialogs/ErrorDialog.tsx
@@ -1,8 +1,13 @@
-import { Button, Result } from "antd/lib";
+import { CloseCircleOutlined } from "@ant-design/icons";
+// import Paragraph from "antd/es/skeleton/Paragraph";
+import { Button, Result, Typography } from "antd/lib";
+
+const { Paragraph, Text } = Typography;
 
 const ErrorDialog = ({ error, onClose }) => {
   return (
     <Result
+      title="Oops!"
       status="500"
       subTitle={
         <>
@@ -15,7 +20,32 @@ const ErrorDialog = ({ error, onClose }) => {
         </Button>
       }
       style={{ margin: "0 auto" }}
-    />
+    >
+      <div className="desc">
+        <Paragraph>
+          <Text
+            strong
+            style={{
+              fontSize: 16,
+            }}
+          >
+            The content you submitted has the following error:
+          </Text>
+          <Paragraph>
+            <CloseCircleOutlined style={{ color: "red" }} /> {error}{" "}
+            <a>Reflesh page &gt;</a>
+          </Paragraph>
+        </Paragraph>
+        <Paragraph>
+          <CloseCircleOutlined style={{ color: "red" }} /> Your account has been
+          suspended. <a>Thaw immediately &gt;</a>
+        </Paragraph>
+        <Paragraph>
+          <CloseCircleOutlined style={{ color: "red" }} /> Your account is not
+          yet eligible to view this pagee. <a>Register Unlock &gt;</a>
+        </Paragraph>
+      </div>
+    </Result>
   );
 };
 
