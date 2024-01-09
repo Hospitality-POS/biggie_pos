9e7e6f90f0c99a18dc2da91aa609f3845b758485
mik284 <mikeykamau222@gmail.com>
2024-01-09 18:02:54 +0300

    add path from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
         sourcemap: true,
       },
     }),
+    tsconfigPaths()
   ],
 })
