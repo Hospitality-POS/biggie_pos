99a95a97eb2b648b3c631d59f1b2dd150780d00d
mik284 <mikeykamau222@gmail.com>
2024-02-18 23:14:29 +0300

    updated category service

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,5 +1,6 @@
 import { ParamsType } from "@ant-design/pro-components";
 import { BASE_URL } from "@utils/config";
+import { Modal, notification } from "antd/lib";
 
 import axios from "axios";
 
@@ -19,9 +20,30 @@ export const fetchSubCategories = async () => {
 
 export const fetchMainCategories = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/categories/main-categories`);   
+    const response = await axios.get(`${BASE_URL}/categories/main-categories`);
     return response.data;
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const editCategory = async (params: ParamsType) => {
+  console.log(params);
+  
+  const url = `${BASE_URL}/categories`;
+  try {
+    const response = await axios.put(`${url}/${params?.data?._id}`, params?.values);
+    notification.success({
+      message: `Success`,
+      description: "Successfully edited category",
+      placement: "bottomLeft",
+    });
+    return response.data;
+  } catch (error) {
+     Modal.error({
+       title: `${(error as Error)?.message}`,
+       content: "Please check your internet connection!",
+     }); 
+    return (error as Error).message;
   }
 };
