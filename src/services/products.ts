89a8e1dd44b4b30dccd7977d493c5f4358c79339
mik284 <mikeykamau222@gmail.com>
2024-03-09 00:01:54 +0300

    add product service

---
+++ src/services/products.ts
@@ -0,0 +1,32 @@
+import SetBearerHeaderToken from "@utils/SetBearerHeaderToken";
+import { BASE_URL } from "@utils/config";
+import { Modal } from "antd";
+import axios from "axios";
+
+const productUrl = `${BASE_URL}/product`;
+
+const {headers} = SetBearerHeaderToken()
+
+export const getAllProducts = async () => {
+  try {
+    const response = await axios.get(`${productUrl}/products/getproducts/all`);
+    return response.data;
+  } catch (error) {
+    Modal.error({
+      title: `${error?.message}`,
+      content: "Please check your internet connection!",
+    });
+  }
+};
+
+export const deleteProduct = async (productId: string) => {
+  try {
+    await axios.delete(`${productUrl}/${productId}`, { headers });
+    return productId;
+  } catch (error) {
+    Modal.error({
+      title: `${error?.message}`,
+      content: "Please check your internet connection!",
+    });
+  }
+};
