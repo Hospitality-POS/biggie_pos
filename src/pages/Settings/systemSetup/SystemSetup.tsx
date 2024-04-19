372e753e54b52902931697b32caf36edb81f3bb9
mik284 <mikeykamau222@gmail.com>
2024-04-19 17:26:21 +0300

    update system ui cards

--- src/pages/Settings/systemSetup/SystemSetup.tsx
+++ src/pages/Settings/systemSetup/SystemSetup.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ProCard } from "@ant-design/pro-components";
+import CategorySettings from "./CategorySettings";
+import { Space } from "antd/lib";
+import { ApartmentOutlined, HolderOutlined, SettingOutlined } from "@ant-design/icons";
+import SubCategorySettings from "./Sub_category";
+import MainCategorySettings from "./Main_category";
+import { ConfigProvider, Typography } from "antd";
 
-function SystemSetup() {
+const Category2 = () => <div>Content for Category 2</div>;
+const MainCategory = () => <div>Main Category Content</div>;
+
+const SystemSetup: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("profile");
+
+  const handleTabChange = (key: string) => {
+    setActiveTab(key);
+  };
+
+  const tabsItems = [
+    {
+      key: "profile",
+      tab: "profile",
+      label: (
+        <>
+          <HolderOutlined />
+         Profile
+        </>
+      ),
+      children: Category2(),
+    },
+    {
+      key: "billing",
+      tab: "billing",
+      label: (
+        <>
+          <HolderOutlined />
+         Billing
+        </>
+      ),
+      children: MainCategory(),
+    },
+  ];
+
   return (
-    <div>SystemSetup</div>
-  )
-}
+    <ConfigProvider
+      theme={{
+        components: {
+          Tabs: {
+            itemColor: "#fff",
+            itemActiveColor: "#000",
+            itemHoverColor: "#aa846f",
+            itemSelectedColor: "#000",
+            cardBg: "#6c1c2c",
+          },
+        },
+      }}
+    >
+      <ProCard
+        tabs={{
+          type: "card",
+          items: tabsItems,
+          activeKey: activeTab,
+          tabBarGutter: 5,
+          onChange: handleTabChange,
+          tabPosition: "left",
+        }}
+        title={
+          <Typography.Title level={4}>
+            <SettingOutlined /> System Setup
+          </Typography.Title>
+        }
+        bordered
+        boxShadow
+      />
+    </ConfigProvider>
+  );
+};
 
-export default SystemSetup
+export default SystemSetup;
