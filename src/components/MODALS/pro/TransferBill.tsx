71ce7f601d28619768f2f5efe35e1094df533565
mik284 <mikeykamau222@gmail.com>
2024-05-14 23:40:05 +0300

    add refetch fn

--- src/components/MODALS/pro/TransferBill.tsx
+++ src/components/MODALS/pro/TransferBill.tsx
@@ -1,21 +1,30 @@
 import React, { useRef } from "react";
 import { Button, Form, Space } from "antd";
 import { ModalForm, ProForm, ProFormDigit } from "@ant-design/pro-form";
-import { CarryOutOutlined, DollarCircleOutlined, SwapOutlined, TableOutlined } from "@ant-design/icons";
+import {
+  CarryOutOutlined,
+  DollarCircleOutlined,
+  SwapOutlined,
+  TableOutlined,
+} from "@ant-design/icons";
 import { transferBill } from "@services/bills";
 import ShowConfirm from "@utils/ConfirmUtil";
 import { ProFormSelect, ProFormTreeSelect } from "@ant-design/pro-components";
 import { fetchTableUsequery, transferCartitems } from "@services/tables";
 import { useQuery } from "@tanstack/react-query";
 import { useAppDispatch, useAppSelector } from "src/store";
-import { useNavigate } from "react-router-dom";
-import { deleteCartItem } from "@features/Cart/CartActions";
+import { useNavigate, useParams } from "react-router-dom";
+import { createCart, deleteCartItem } from "@features/Cart/CartActions";
+import useCartItemsData from "@hooks/cartItemsData";
+import { reset } from "@features/Auth/AuthSlice";
 
 interface TransferBillModalProps {
   data?: any;
 }
 
-const TransferBillModal: React.FC<TransferBillModalProps> = ({ data : cartItem }) => {
+const TransferBillModal: React.FC<TransferBillModalProps> = ({
+  data: cartItem,
+}) => {
   const { data } = useQuery({
     queryKey: ["tables"],
     queryFn: fetchTableUsequery,
@@ -26,28 +35,33 @@ const TransferBillModal: React.FC<TransferBillModalProps> = ({ data : cartItem }
   const [form] = Form.useForm();
   const formRef = useRef();
   const navigate = useNavigate();
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const { data: cartItems, refetch } = useCartItemsData();
+
+  const { id } = useParams();
 
   const locations = data?.map(
     (table: { name: string; _id: string; tables: any[] }) => ({
       title: table?.name,
       value: table?._id,
       disabled: true,
-      children: table?.tables.map(
-        (childTable: { name: string; _id: string }) => ({
+      children: table?.tables
+        .filter(
+          (childTable: { name: string; _id: string }) => childTable._id !== id
+        ) // Efficient filtering
+        .map((childTable) => ({
           title: childTable?.name,
           value: childTable?._id,
           icon: <CarryOutOutlined />,
-        })
-      ),
+        })),
     })
   );
-  
+
   const productOptions = cartItem?.map((product) => ({
     label: product.product_id?.name,
     value: product._id,
   }));
-  
+
   return (
     <ModalForm
       form={form}
@@ -81,19 +95,15 @@ const TransferBillModal: React.FC<TransferBillModalProps> = ({ data : cartItem }
       }}
       onFinish={async (values) => {
         console.log("transfer bilss ddata", values);
-        
+
         const confirmed = await ShowConfirm({
           title: "Are you sure you want to transfer this bill?",
         });
         if (confirmed) {
-            await transferCartitems(values);
-            // values.products.forEach((productId) =>
-            //   dispatch(deleteCartItem(productId))
-            // );
-            if (!cartItem.length){
-              navigate("/tables");
-            }
-             return true;
+          await transferCartitems(values);
+          refetch();
+          navigate("/tables")
+          return true;
         }
       }}
       submitter={{
@@ -120,14 +130,17 @@ const TransferBillModal: React.FC<TransferBillModalProps> = ({ data : cartItem }
           name="table"
           label="Tables"
           rules={[
-            { required: true, message: "Please select a table location to transfer!" },
+            {
+              required: true,
+              message: "Please select a table location to transfer!",
+            },
           ]}
           request={() => locations}
           allowClear
           fieldProps={{
             treeLine: true,
             suffixIcon: <TableOutlined />,
-             treeIcon: true,
+            treeIcon: true,
             filterTreeNode: true,
             showSearch: true,
             popupMatchSelectWidth: false,
