ca302d5ceb8b8747682161b9c712bfd45d1d2d91
mik284 <mikeykamau222@gmail.com>
2024-05-14 23:41:39 +0300

    add & modify hook for cartitems

---
+++ src/hooks/cartItemsData.ts
@@ -0,0 +1,33 @@
+import { getAllCartItems } from "@services/cart";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useAppSelector } from "src/store";
+
+function useCartItemsData() {
+     const {
+       cartDetails,
+    //    totalAmount,
+    //    cartItems: data2,
+    //    loading,
+     } = useAppSelector((state) => state.cart);
+
+     const queryKey = useMemo(
+       () => ["cartItems", cartDetails?._id],
+       [cartDetails?._id]
+     );
+
+  const { data, isLoading, refetch } = useQuery(
+    queryKey,
+    async () => getAllCartItems(cartDetails?._id),
+    {
+      onError: (error) => console.error("Error fetching cart items:", error),
+      enabled: !!cartDetails?._id,
+      refetchOnWindowFocus: false,
+    }
+  );
+  return {
+    data, isLoading, refetch
+  };
+}
+
+export default useCartItemsData;
