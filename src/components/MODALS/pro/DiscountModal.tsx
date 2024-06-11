d8b6a5663c9a01534043a1dabc1ee05c58a3f33e
mik284 <mikeykamau222@gmail.com>
2024-06-11 17:06:19 +0300

    moved dicount button to payment drawer

--- src/components/MODALS/pro/DiscountModal.tsx
+++ src/components/MODALS/pro/DiscountModal.tsx
@@ -37,7 +37,7 @@ const DiscountModal: React.FC<DiscountModalProps> = ({ data: cartItem }) => {
         </Space>
       }
       trigger={
-        <Button type="primary" icon={<PercentageOutlined />}>
+        <Button type="primary" icon={<PercentageOutlined />} block>
         Offer Discount?
         </Button>
       }
