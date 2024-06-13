a72a44b7f0e60a5eb3fd30f583d6a152b8de5736
mik284 <mikeykamau222@gmail.com>
2024-06-13 10:23:52 +0300

    add dispatch to clinet pin modal

--- src/components/MODALS/ClientPin.tsx
+++ src/components/MODALS/ClientPin.tsx
@@ -1,16 +1,23 @@
 import { KeyOutlined } from "@ant-design/icons";
 import ProForm, { ModalForm, ProFormText } from "@ant-design/pro-form";
+import { updateCart } from "@features/Cart/CartActions";
 import { addClientPin } from "@features/Cart/CartSlice";
+import ShowConfirm from "@utils/ConfirmUtil";
 import { Button, Form, Space, Typography } from "antd";
 import { useAppDispatch } from "src/store";
 
-
-function ClientPin() {
-    const dispatch = useAppDispatch();
+function ClientPin({ cart: cartDetails }) {
+  const dispatch = useAppDispatch();
   const [form] = Form.useForm();
+  
   return (
     <ModalForm
-    title={<Space><KeyOutlined/><Typography>Enter Client Pin</Typography></Space>}
+      title={
+        <Space>
+          <KeyOutlined />
+          <Typography>Enter Client Pin</Typography>
+        </Space>
+      }
       form={form}
       width={400}
       modalProps={{
@@ -32,17 +39,18 @@ function ClientPin() {
         </Button>
       }
       onFinish={async (values) => {
-        console.log(values);
-        dispatch(addClientPin(values.clientPin));
-        return true;
+        const confirmed = await ShowConfirm({
+          title: "Confirm if the client pin is correct?",
+          position: true,
+        });
+        if (confirmed) {
+          dispatch(updateCart({ cart: cartDetails, data: values }));
+          return true;
+        }
       }}
     >
       <ProForm.Group>
-        <ProFormText
-          width="md"
-          name="clientPin"
-          placeholder="A**********"
-        />
+        <ProFormText width="md" name="client_pin" placeholder="A**********" />
       </ProForm.Group>
     </ModalForm>
   );
