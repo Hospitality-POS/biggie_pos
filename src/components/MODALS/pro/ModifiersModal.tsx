12aea9dbdff8bc4d3a58ff10dea6c03b5f058726
mik284 <mikeykamau222@gmail.com>
2024-07-03 08:50:56 +0300

    feat: add modifiers modal

---
+++ src/components/MODALS/pro/ModifiersModal.tsx
@@ -0,0 +1,93 @@
+import React, { useRef } from "react";
+import { Button, Form, Space } from "antd";
+import { ModalForm, ProFormText, ProForm } from "@ant-design/pro-form";
+import { CrownOutlined, EditOutlined, PushpinOutlined } from "@ant-design/icons";
+import ShowConfirm from "@utils/ConfirmUtil";
+import { addNewMainCategory, editMainCategory } from "@services/categories";
+
+interface ModifiersModalProps {
+  actionRef: any;
+  edit?: boolean;
+  data?: any;
+}
+
+const ModifiersModal: React.FC<ModifiersModalProps> = ({
+  actionRef,
+  edit,
+  data,
+}) => {
+  const [form] = Form.useForm();
+  const formRef = useRef();
+
+  return (
+    <ModalForm
+      width={550}
+      layout="horizontal"
+      title={
+        <Space>
+          <PushpinOutlined />
+          {edit ? "Edit Modifiers" : "Add New Modifiers"}
+        </Space>
+      }
+      initialValues={edit ? { ...data } : {}}
+      trigger={
+        edit ? (
+          <Button
+            type="link"
+            key="button"
+            icon={
+              <EditOutlined
+                style={{ color: "#6c1c2c" }}
+                onClick={() => form.setFieldsValue(data)}
+              />
+            }
+          ></Button>
+        ) : (
+          <Button key="button" icon={<PushpinOutlined />}>
+            Add New Modifiers
+          </Button>
+        )
+      }
+      autoFocusFirstInput
+      modalProps={{
+        destroyOnClose: true,
+        style: { display: "grid", placeContent: "center" },
+      }}
+      onFinish={async (values) => {
+        const confirmed = await ShowConfirm({
+          title: `Are you sure you want to ${
+            edit ? "update this" : "add new"
+          } modifiers?`,
+        });
+        if (confirmed) {
+          edit
+            ? await editMainCategory({ values, _id: data?._id })
+            : await addNewMainCategory(values);
+          actionRef.current.reset();
+          return true;
+        }
+      }}
+      onOpenChange={(visible) => !visible}
+      form={form}
+      formRef={formRef}
+      submitter={{
+        searchConfig: {
+          resetText: "Cancel",
+          submitText: edit ? "Edit Modifiers" : "Add Modifiers",
+        },
+      }}
+    >
+      <ProForm.Group>
+        <ProFormText
+          width="md"
+          name="name"
+          label="Create New Modifiers"
+          rules={[{ required: true, message: "Modifiers Name is required" }]}
+          placeholder="Enter Modifiers Name"
+        />
+      </ProForm.Group>
+    </ModalForm>
+  );
+};
+
+export default ModifiersModal;
