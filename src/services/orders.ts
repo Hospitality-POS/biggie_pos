cf5afa09b9ad93eff1f36e546ea64ec64696a6b6
mik284 <mikeykamau222@gmail.com>
2024-09-22 17:57:15 +0300

    update settings services

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -1,5 +1,6 @@
 import { ParamsType } from "@ant-design/pro-components";
 import { BASE_URL } from "@utils/config";
+import { message } from "antd";
 import axios from "axios";
 
 export const getAllOrders = async (data: ParamsType) => {
@@ -13,5 +14,18 @@ export const getAllOrders = async (data: ParamsType) => {
     return response.data;
   } catch (error) {
     console.log(error);
+  }
+};
+
+
+export const deleteOrderById = async (id: string) => {
+  try {
+    await axios.delete(`${BASE_URL}/orders/${id}`);
+    message.success("Order deleted successfully");
+    return true;
+  } catch (error) {
+    console.log(error);
+    message.error("Error deleting order");
+    return false;
   }
 };
