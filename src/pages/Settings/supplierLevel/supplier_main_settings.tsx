018b390f3127810762425cb0f14a31d662a72367
kinuthiakaranja0@gmail.com <kinuthiakaranja0@gmail.com>
2024-10-21 22:14:47 +0300

    staff shift schedule

--- src/pages/Settings/supplierLevel/supplier_main_settings.tsx
+++ src/pages/Settings/supplierLevel/supplier_main_settings.tsx
@@ -2,6 +2,7 @@ import { ProCard } from "@ant-design/pro-components";
 import SupplierTable from "./Supplier";
 import { SisternodeOutlined } from "@ant-design/icons";
 import { Space } from "antd/lib";
+import EmployeeShiftSchedule from "./Employee";
 
 function SupplierMainSettings() {
   const tabsItems = [
@@ -11,12 +12,18 @@ function SupplierMainSettings() {
       label: "All Suppliers",
       children: <SupplierTable />,
     },
+    {
+      key: "table2",
+      tab: "Table 2",
+      label: "Employee ",
+      children: <EmployeeShiftSchedule />,
+    },
   ];
   return (
     <>
       <ProCard
         // style={{ height: "90vh" }}
-        title={<Space><SisternodeOutlined/>Supplier Main Settings</Space>}
+        title={<Space><SisternodeOutlined />Supplier Main Settings</Space>}
         tabs={{
           type: "card",
           items: tabsItems,
