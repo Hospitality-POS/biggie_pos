c58d2289788b97d1d8aab949a4e79ba10b9c44bd
mik284 <mikeykamau222@gmail.com>
2024-10-26 23:11:54 +0300

    add uom service

---
+++ src/services/uom.ts
@@ -0,0 +1,62 @@
+import { ParamsType } from "@ant-design/pro-components";
+import { BASE_URL } from "@utils/config";
+import { message } from "antd";
+import axios, { AxiosError } from "axios";
+
+const uomUrl = `${BASE_URL}/uom`;
+
+interface Uom {
+  name: string;
+  _id: string;
+}
+
+export const fetchAllUom = async (params: ParamsType) => {
+  try {
+    const response = await axios.get(uomUrl, { params });
+    return response.data;
+  } catch (error: unknown) {
+    const err = error as AxiosError;
+    console.error("Error fetching uom:", err.message);
+    message.error("Failed to fetch uom");
+    throw err;
+  }
+};
+
+export const createUom = async (uomData: Uom) => {
+  try {
+    const response = await axios.post(uomUrl, uomData);
+    message.success("UOM created successfully");
+    return response.data;
+  } catch (error: unknown) {
+    const err = error as AxiosError;
+    console.error("Error creating uom:", err.message);
+    message.error("Failed to create uom");
+    throw err;
+  }
+};
+
+export const updateUom = async (uomData: Uom) => {
+  try {
+    const response = await axios.put(`${uomUrl}/${uomData._id}`, uomData);
+    message.success("UOM updated successfully");
+    return response.data;
+  } catch (error: unknown) {
+    const err = error as AxiosError;
+    console.error("Error updating uom:", err.message);
+    message.error("Failed to update uom");
+    throw err;
+  }
+};
+
+export const deleteUom = async (id: string) => {
+  try {
+    const response = await axios.delete(`${uomUrl}/${id}`);
+    message.success("UOM deleted successfully");
+    return response.data;
+  } catch (error: unknown) {
+    const err = error as AxiosError;
+    console.error("Error deleting uom:", err.message);
+    message.error("Failed to delete uom");
+    throw err;
+  }
+};
