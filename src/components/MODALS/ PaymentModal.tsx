9c75b4779ea8c469d9a3d69e3efd5bc2c58ddf24
kinuthiakaranja0@gmail.com <kinuthiakaranja0@gmail.com>
2024-12-12 12:53:59 +0300

    change sub

--- src/components/MODALS/ PaymentModal.tsx
+++ src/components/MODALS/ PaymentModal.tsx
@@ -1,87 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Button, Space, Spin, Typography } from 'antd';
-import { MobileOutlined, CreditCardOutlined } from '@ant-design/icons';
-import ProForm, { ModalForm, ProFormText } from "@ant-design/pro-form";
-
-const { Text } = Typography;
-
-const PaymentModal = ({
-    open,
-    handleOpenChange,
-    handleStkPush,
-    form,
-    formRef,
-    selectedPaymentMethod,
-    setSelectedPaymentMethod,
-    isComingSoon,
-    loadingPayment,
-}) => {
-    return (
-        <ModalForm
-            onOpenChange={handleOpenChange}
-            visible={open}
-            onFinish={handleStkPush}
-            form={form}
-            formRef={formRef}
-            title="Select Payment Method"
-            submitter={{
-                searchConfig: {
-                    resetText: "Cancel",
-                    submitText: "Complete Payment",
-                },
-                render: (props, dom) => (
-                    <Space style={{ justifyContent: 'space-between' }}>
-                        {dom}
-                    </Space>
-                ),
-            }}
-            style={{ maxWidth: '600px', margin: '0 auto' }} // Center the modal
-        >
-            <Space direction="vertical" style={{ width: '100%', padding: '20px 0' }}>
-                <Button
-                    type="primary"
-                    icon={<MobileOutlined />}
-                    size="large"
-                    block
-                    onClick={() => setSelectedPaymentMethod('mpesa')}
-                    style={{ marginBottom: '16px' }}
-                >
-                    Pay with M-PESA (STK Push)
-                </Button>
-
-                <Button
-                    icon={<CreditCardOutlined />}
-                    size="large"
-                    block
-                    disabled={isComingSoon}
-                    style={{ marginBottom: '16px' }}
-                >
-                    {isComingSoon ? 'Coming Soon' : 'Pay with Card'}
-                </Button>
-
-                {selectedPaymentMethod === 'mpesa' && (
-                    <ProForm.Group style={{ width: "100%" }}>
-                        <ProFormText
-                            width="lg"
-                            name="phoneNumber"
-                            label="Phone Number"
-                            rules={[{ required: true, message: "Phone Number is required" }]}
-                            placeholder="Phone Number (254...)"
-                            style={{ marginBottom: '16px' }} // Add margin for spacing
-                        />
-                    </ProForm.Group>
-                )}
-
-                {loadingPayment && (
-                    <Spin spinning={loadingPayment} size="large" style={{ marginTop: '20px' }}>
-                        <Text>Processing payment...</Text>
-                    </Spin>
-                )}
-            </Space>
-        </ModalForm>
-    );
-=======
 import React, { useEffect, useRef, useState } from "react";
 import { Button, Space, Spin, Typography, Card, Alert, Form } from "antd";
 import {
@@ -289,7 +205,6 @@ const PaymentModal: React.FC<PaymentModalProps> = ({ data, actionRef }) => {
       </Space>
     </ModalForm>
   );
->>>>>>> 64f9563f3b8d5b13d219dbb7825c121c49673886
 };
 
 export default PaymentModal;
--- src/components/MODALS/ChangeSubscription.tsx
+++ src/components/MODALS/ChangeSubscription.tsx
@@ -1,71 +1,139 @@
-import React, { useState } from 'react';
-import { Modal, Space, Radio, Card, Button, Typography, message } from 'antd';
+import React, { useEffect, useRef, useState } from "react";
+import { Space, Radio, Card, Button, Typography, message, Form } from "antd";
 import { updateSubscription } from "@services/users";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
+import { ModalForm } from "@ant-design/pro-form";
+import { SwapOutlined, WalletOutlined } from "@ant-design/icons";
+import Title from "antd/es/typography/Title";
+
+interface ChangeSubscriptionModalProps {
+  tenant: any;
+  actionRef: any;
+}
 
 const { Text } = Typography;
 
-const ChangeSubscription = ({ tenant, isModalVisible, setIsModalVisible }) => {
+const ChangeSubscriptionModal: React.FC<ChangeSubscriptionModalProps> = ({
+  tenant,
+  actionRef,
+}) => {
   const [selectedPlan, setSelectedPlan] = useState(tenant.subscription_id._id);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form] = Form.useForm();
+  const formRef = useRef();
+  const [open, setOpen] = useState(false);
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+    if (!newOpen) {
+      form.resetFields();
+    }
+  };
+
+  useEffect(() => {
+    if (open && tenant) {
+      form.setFieldsValue({
+        ...tenant,
+      });
+    }
+  }, [open, tenant, form]);
+
+  const isDisabled = tenant.invoices && tenant.invoices.length > 0;
 
   const dispatch = useDispatch();
 
-
   const handleSubmit = async () => {
-    setIsSubmitting(true);
-
     const storedCode = localStorage.getItem("companyCode");
     const payload = {
       tenantId: tenant._id,
       companyCode: storedCode,
-      subscription_id: selectedPlan
+      subscription_id: selectedPlan,
     };
 
     try {
       await dispatch(updateSubscription(payload)); // Wait for the updateSubscription to complete
       message.success("Account subscription package updated successfully");
+      setOpen(false);
+      actionRef.current.reset();
+      return true;
     } catch (error) {
-      message.error("Account subscription package update failed");
-    } finally {
-      setIsModalVisible(false); // Close the modal after the request is complete
-      setIsSubmitting(false);
+      console.log("Account subscription package update failed", error);
+      return false;
     }
   };
 
   return (
-    <Modal
-      title="Change Subscription Plan"
-      visible={isModalVisible}
-      onCancel={() => setIsModalVisible(false)}
-      footer={null}
-      width={600}
+    <ModalForm
+      trigger={
+        <Button type="primary" icon={<SwapOutlined />} disabled={isDisabled}>
+          Change Plan
+        </Button>
+      }
+      title={
+        <Space>
+          <SwapOutlined />
+          <Title level={4} style={{ margin: 0 }}>
+            Change Subscription Plan
+          </Title>
+        </Space>
+      }
+      open={open}
+      onOpenChange={handleOpenChange}
+      form={form}
+      formRef={formRef}
+      style={{ padding: "24px", backgroundColor: "#f5f5f5" }}
+      modalProps={{
+        destroyOnClose: true,
+        centered: true,
+        width: 700,
+      }}
+      submitter={{
+        searchConfig: {
+          resetText: "Cancel",
+          submitText: "Confirm Change",
+        },
+        render: (props, dom) => (
+          <Space
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {dom}
+          </Space>
+        ),
+      }}
+      onFinish={handleSubmit}
     >
-      <Space direction="vertical" style={{ width: '100%', padding: '20px 0' }}>
+      <Space direction="vertical" style={{ width: "100%", padding: "20px 0" }}>
         <Radio.Group
-          style={{ width: '100%' }}
+          style={{ width: "100%" }}
           value={selectedPlan}
           onChange={(e) => setSelectedPlan(e.target.value)}
         >
-          <Space direction="horizontal" style={{ width: '100%' }}>
-            {tenant.subscriptions.map((plan) => (
+          <Space direction="horizontal" style={{ width: "100%" }}>
+            {tenant.subscriptions.map((plan: any) => (
               <Radio
                 key={plan._id}
                 value={plan._id}
-                checked={plan._id.toString() === tenant.subscription_id._id.toString()}
-                style={{ width: '100%' }}
+                checked={
+                  plan._id.toString() === tenant.subscription_id._id.toString()
+                }
+                style={{ width: "100%" }}
               >
-                <Card style={{ width: '100%', marginTop: '8px' }}>
+                <Card style={{ width: "100%", marginTop: "8px" }}>
                   <Space direction="vertical">
                     <Text strong>{plan.name}</Text>
-                    {plan.price.map((pricing) =>
-                      Object.entries(pricing).map(([billingCycle, amount]) => (
-                        billingCycle === tenant.subscription_cycle && (
-                          <Text type="secondary" key={billingCycle}>
-                            {billingCycle === 'yearly' ? `Yearly: ${amount} Kes` : `Monthly:  ${amount} Kes`}
-                          </Text>
-                        )
-                      ))
+                    {plan.price.map((pricing: any) =>
+                      Object.entries(pricing).map(
+                        ([billingCycle, amount]) =>
+                          billingCycle === tenant.subscription_cycle && (
+                            <Text type="secondary" key={billingCycle}>
+                              {billingCycle === "yearly"
+                                ? `Yearly: ${amount} Kes`
+                                : `Monthly:  ${amount} Kes`}
+                            </Text>
+                          )
+                      )
                     )}
                   </Space>
                 </Card>
@@ -73,18 +141,9 @@ const ChangeSubscription = ({ tenant, isModalVisible, setIsModalVisible }) => {
             ))}
           </Space>
         </Radio.Group>
-        <Button
-          type="primary"
-          block
-          onClick={handleSubmit}
-          loading={isSubmitting}
-          style={{ marginTop: '20px' }}
-        >
-          Confirm Change
-        </Button>
       </Space>
-    </Modal>
+    </ModalForm>
   );
 };
 
-export default ChangeSubscription;
+export default ChangeSubscriptionModal;
