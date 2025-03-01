ddfa5e3ecb702767376c49622b932336875c90f7
kinuthiakaranja0@gmail.com <kinuthiakaranja0@gmail.com>
2025-03-01 20:05:21 +0300

    gift cards

--- src/services/customers.ts
+++ src/services/customers.ts
@@ -2,9 +2,13 @@ import { ParamsType } from "@ant-design/pro-components";
 import { BASE_URL } from "@utils/config";
 import { message } from "antd";
 import axiosInstance from "./request";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 const categ_url = `${BASE_URL}/customers`;
 
+
 //  categories
+
+
 
 export const fetchAllCustomers = async (data: ParamsType) => {
     try {
@@ -67,4 +71,45 @@ export const logCustomerVisit = async (params: ParamsType) => {
         console.log('error', error);
     }
 };
+
+export const fetchAllGiftCards = async (data: any) => {
+    try {
+        console.log('wewe', data);
+        const url = `${categ_url}/all-cards`;
+
+        const response = await axiosInstance.get(url, {
+            params: { customer_id: data },
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(error?.message);
+    }
+};
+
+export const createGiftCard = createAsyncThunk(
+    'giftCard/create',
+    async (giftCardData, { rejectWithValue }) => {
+        try {
+            const response = await axiosInstance.post(categ_url + '/new-card', giftCardData);
+            return response.data;
+        } catch (error: any) {
+            message.error(error?.message || "Failed to create gift card");
+            return rejectWithValue(error?.message || "Failed to create gift card");
+        }
+    }
+);
+
+// Send gift card email thunk
+export const sendGiftCard = createAsyncThunk(
+    'giftCard/send',
+    async (data, { rejectWithValue }) => {
+        try {
+            const response = await axiosInstance.post(categ_url + '/new-card-email', data);
+            return response.data;
+        } catch (error: any) {
+            message.error(error?.message || "Failed to send gift card email");
+            return rejectWithValue(error?.message || "Failed to send gift card email");
+        }
+    }
+);
 
