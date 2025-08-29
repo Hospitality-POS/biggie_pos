ef8e759c97e92dd85e8244c448bdaee4f76b2114
mik284 <mikeykamau222@gmail.com>
2025-08-29 23:48:22 +0300

    add get color util

---
+++ src/utils/getPrimaryColor.ts
@@ -0,0 +1,11 @@
+export const getPrimaryColor = (): string => {
+    try {
+        const storedTenant = localStorage.getItem("tenant");
+        const tenant = storedTenant ? JSON.parse(storedTenant) : null;
+
+        return tenant?.color_scheme?.primary || "#6c1c2c";
+    } catch (error) {
+        console.error("Error reading tenant from localStorage:", error);
+        return "#6c1c2c";
+    }
+};
