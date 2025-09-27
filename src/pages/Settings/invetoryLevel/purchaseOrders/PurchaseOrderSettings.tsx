573300cd3424a7283f8543b8dfd8a3066ffded1f
mik284 <mikeykamau222@gmail.com>
2025-09-27 05:06:39 +0300

    Introduced export functionality for Excel and PDF formats.

---
+++ src/pages/Settings/invetoryLevel/purchaseOrders/PurchaseOrderSettings.tsx
@@ -0,0 +1,45 @@
+import { PurchaseOrderTable } from './components/PurchaseOrderTable';
+import { usePurchaseOrders } from './hooks/usePurchaseOrders';
+import PrintPreviewModal from '../PrintPreviewModal';
+
+export const PurchaseOrderSettings = () => {
+  const {
+    loading,
+    printModal,
+    fetchPurchaseOrders,
+    deletePurchaseOrder,
+    updateStatus,
+    exportToExcel,
+    exportToPDF,
+    showPrintPreview,
+    showBulkPrintPreview,
+    closePrintModal,
+    generateSummary
+  } = usePurchaseOrders();
+
+  return (
+    <>
+      <PurchaseOrderTable
+        loading={loading}
+        onFetchData={fetchPurchaseOrders}
+        onDeletePO={deletePurchaseOrder}
+        onUpdateStatus={updateStatus}
+        onPrintPO={showPrintPreview}
+        onExportToExcel={exportToExcel}
+        onExportToPDF={exportToPDF}
+        onBulkPrint={showBulkPrintPreview}
+        onGenerateSummary={generateSummary}
+      />
+
+      <PrintPreviewModal
+        visible={printModal.visible}
+        onClose={closePrintModal}
+        data={printModal.data}
+        type={printModal.type}
+        title={printModal.title}
+      />
+    </>
+  );
+};
+
+export default PurchaseOrderSettings;
